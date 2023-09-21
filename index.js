ae918d3e2fe562ce2710566bf16a6cf8f3ec2932
Rithik Singh <69286842+Rithik-dotcom@users.noreply.github.com>
2023-09-21 21:30:22 +0000

    practised buy cake redux

--- index.js
+++ index.js
@@ -1,90 +1,45 @@
 
-// const redux = require('redux')
-// const createStore = redux.createStore
-
-// //creating action
-// const buy_cake = "buy_cake"
-
-// function buycake() {
-//     return {
-//         type: buy_cake,
-//         info: "first redux action"
-//     }
-// }
-
-// //create reducer
-// const initialState = {
-//     noOfCakes: 10,
-// }
-
-// const reducer = (state = initialState, action) => {
-//     switch (action.type) {
-//         case buy_cake: return {
-//             ...state,
-//             noOfCakes: state.noOfCakes - 1,
-//         }
-
-//         default: return state
-
-//     }
-// }
-
-// //creating store
-// const store = createStore(reducer)
-// console.log('initial state', store.getState()) //get state to get a state
-
-// const unsubscribe = store.subscribe(() => console.log("updated state:", store.getState()))//listener using subscribe
-// store.dispatch(buycake()) // dispatch to implement action
-
-// unsubscribe()
-
+const redux = require('redux')
+const createStore = redux.createStore
 
 //creating action
-const redux = require('redux');
-const createStore = redux.createStore;
+const buy_cake = "buy_cake"
 
-
-const buy_cake = "buy a cake"
-
-
-function buyCake() {
-    return{
+function buycake() {
+    return {
         type: buy_cake,
-        info: "buy a cake action"
+        info: "first redux action"
     }
 }
 
-//creating a reducer and initial state
+//create reducer
 const initialState = {
-    numberOfcakes: 10
+    noOfCakes: 10,
 }
-
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case buy_cake: return {
             ...state,
-            numberOfcakes: state.numberOfcakes - 1
+            noOfCakes: state.noOfCakes - 1,
         }
+
         default: return state
+
     }
 }
 
-
 //creating store
+const store = createStore(reducer)
+console.log('initial state', store.getState()) //get state to get a state
 
-const store = createStore(reducer)  //creating store
-console.log("initial state", store.getState())  //getting state
+const unsubscribe = store.subscribe(() => console.log("updated state:", store.getState()))//listener using subscribe
+store.dispatch(buycake()) // dispatch to implement action
 
-const unsubscribe = store.subscribe(()=>{console.log('updated state', store.getState())})  //listener 
-
-store.dispatch(buyCake()) // action event
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+unsubscribe()
 
 
-unsubscribe() // unsubscribing from the lister end after dispatch
+
 
 
 
